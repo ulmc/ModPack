6643abcbddc33df625c3987dfa20290d66c904fa
bormoshka <bormoshka@Robotic-IV>
2014-01-15 22:50:41 +0400

    Added Copyrights

--- src/ru/ulmc/extender/tileentity/TileEntityBones.java
+++ src/ru/ulmc/extender/tileentity/TileEntityBones.java
@@ -1,7 +1,26 @@
+/**
+ * Copyright (C) 2014 Kolmogorov Alexey
+ * 
+ * This file part of ulmc.ru ModPack
+ * 
+ * ulmc.ru ModPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ulmc.ru ModPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ * 
+ */
+
 package ru.ulmc.extender.tileentity;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -9,10 +28,7 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet132TileEntityData;
-import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.tileentity.TileEntity;
-import ru.ulmc.extender.UltimateExtender;
-import cpw.mods.fml.common.network.Player;
 
 public class TileEntityBones extends TileEntity implements IInventory {
 	private ItemStack[] inv = new ItemStack[18];
