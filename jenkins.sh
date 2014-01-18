dd13738b66b2ef50c400028ad2316c81430d516c
bormoshka <bormoshka@Robotic-IV>
2014-01-18 21:02:03 +0400

    jenkins script finished

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,12 @@
+rm -r $WORKSPACE/target/
+mkdir $WORKSPACE/target/
 cd /var/jenkins/mcp/
-cp -R $WORKSPACE/src/ru /var/jenkins/mcp/src/
+cp -R $WORKSPACE/src/ru src/minecraft/
 python runtime/recompile.py
 python runtime/reobfuscate.py
-cd reobf/minecraft
-zip -r -D -9 $WORKSPACE/builds/ulmcModPack_$BUILD_NUMBER *
-cd ../..
-cd src/
-zip -r -D -9 $WORKSPACE/builds/ulmcModPack_$BUILD_NUMBER $WORKSPACE/src/mcmod.info
-zip -r -D -9 $WORKSPACE/builds/ulmcModPack_$BUILD_NUMBER $WORKSPACE/src/assets
-cd ..
+cp -R /var/jenkins/mcp/reobf/minecraft/ru $WORKSPACE/target/
+cp -R $WORKSPACE/src/mcmod.info $WORKSPACE/target/
+cp -R $WORKSPACE/LICENSE.txt $WORKSPACE/target/
+cp -R $WORKSPACE/src/assets $WORKSPACE/target/
+cd $WORKSPACE/target/
+zip -r -D -9 $WORKSPACE/artifacts/ulmcModPack_$BUILD_NUMBER *
